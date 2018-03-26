bdd955270b29efadafd97be25f59a0b1c7fb3d11
matej.santrucek <matej.santrucek@W10106.sps-cl.local>
2018-03-26 12:30:48 +0200

    první commit

---
+++ src/ahoj/světe/AhojSvěte.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ahoj.světe;
+
+/**
+ *
+ * @author matej.santrucek
+ */
+public class AhojSvěte {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("nazdar");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + " ");
+        }
+    }
+
+}
